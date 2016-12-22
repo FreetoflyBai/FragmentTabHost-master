fc41708d2c73693ad943215b2f95557e2be07094
kevin.bai <12354*.com>
2016-12-22 17:50:50 +0800

    modify readme.md

--- app/src/main/java/com/android/tabhost/MainActivity.java
+++ app/src/main/java/com/android/tabhost/MainActivity.java
@@ -31,21 +31,21 @@ public class MainActivity extends AppCompatActivity {
         mTabHost.setTabWidgetPadding(Px2dpUtils.dip2px(this,10));
 
 
-        mTabHost = (FixTabHost) findViewById(R.id.fth);
-        mTabHost.setup(
-                getSupportFragmentManager(),
-                TabEnum.BOTTOM,
-                new String[]{"TAB1","TAB2","TAB3","TAB4","TAB5"},
-                new int[]{R.color.black,R.color.orange},
-                new int[][]{
-                        new int[]{R.drawable.tabbar_letter,R.drawable.tabbar_letter_select},
-                        new int[]{R.drawable.tabbar_newest,R.drawable.tabbar_newest_select},
-                        new int[]{R.drawable.tabbar_pair,R.drawable.tabbar_pair_select},
-                        new int[]{R.drawable.tabbar_mostfire,R.drawable.tabbar_mostfire_select},
-                        new int[]{R.drawable.tabbar_me,R.drawable.tabbar_me_select}},
-                new Class[]{MainFragment.class,MainFragment.class,MainFragment.class,
-                        MainFragment.class,MainFragment.class});
-        mTabHost.setTabWidgetBackground(R.color.tab_widget_background);
+//        mTabHost = (FixTabHost) findViewById(R.id.fth);
+//        mTabHost.setup(
+//                getSupportFragmentManager(),
+//                TabEnum.BOTTOM,
+//                new String[]{"TAB1","TAB2","TAB3","TAB4","TAB5"},
+//                new int[]{R.color.black,R.color.orange},
+//                new int[][]{
+//                        new int[]{R.drawable.tabbar_letter,R.drawable.tabbar_letter_select},
+//                        new int[]{R.drawable.tabbar_newest,R.drawable.tabbar_newest_select},
+//                        new int[]{R.drawable.tabbar_pair,R.drawable.tabbar_pair_select},
+//                        new int[]{R.drawable.tabbar_mostfire,R.drawable.tabbar_mostfire_select},
+//                        new int[]{R.drawable.tabbar_me,R.drawable.tabbar_me_select}},
+//                new Class[]{MainFragment.class,MainFragment.class,MainFragment.class,
+//                        MainFragment.class,MainFragment.class});
+//        mTabHost.setTabWidgetBackground(R.color.tab_widget_background);
 
     }
 }
